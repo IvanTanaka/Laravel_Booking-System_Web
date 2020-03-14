14a707ec9a20768a1bf73a8aa600ecbf259c6f31
Ivan Tanaka <tanakaivan97@yahoo.com>
2020-03-14 16:04:31 +0700

    logo message in login and register page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,20 @@
 
     <!-- Styles -->
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <style>
+        .logo{
+            color: #FF7266;
+            font-weight: 600;
+            font-size: 20px;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Membee') }}
+                <a class="logo" href="{{ url('/') }}">
+                    membee
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
