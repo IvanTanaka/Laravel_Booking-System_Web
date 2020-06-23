83d34d64136a37414e6cf665ae6124ffb629d6f2
Ivan Tanaka <tanakaivan97@yahoo.com>
2020-06-23 16:25:25 +0700

    bank account form

--- resources/views/owner/bank_account/edit.blade.php
+++ resources/views/owner/bank_account/edit.blade.php
@@ -10,7 +10,7 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-12">
-        <h1 class="m-0 text-dark">Edit Bank Account</h1>
+        <h1 class="m-0 text-dark"></h1>
       </div><!-- /.col -->
     </div><!-- /.row -->
   </div><!-- /.container-fluid -->
@@ -20,78 +20,94 @@
 <!-- Main content -->
 <div class="content">
   <div class="container-fluid">
-    <form action="{{ route('bank-account.update',["bank_account"=>$bank_account->id]) }}" method="POST" enctype='multipart/form-data'>
-      @csrf
-      @method('PUT')
-      <div class="row">
-        @if ($message = Session::get('error'))
-        <div class="col-lg-12">
-          <div class="alert alert-danger alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-ban"></i> Error!</h5>
-            {{$message}}
-          </div>
+    <div class="row">
+      @if ($message = Session::get('error'))
+      <div class="col-lg-12">
+        <div class="alert alert-danger alert-dismissible">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+          <h5><i class="icon fas fa-ban"></i> Error!</h5>
+          {{$message}}
         </div>
-        @elseif($errors->any())
-        <div class="col-lg-12">
-          <div class="alert alert-danger alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-ban"></i> Error!</h5>
-            <ul>
-              @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
+      </div>
+      @elseif($errors->any())
+      <div class="col-lg-12">
+        <div class="alert alert-danger alert-dismissible">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+          <h5><i class="icon fas fa-ban"></i> Error!</h5>
+          <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+          </ul>
         </div>
-        @endif
-        <div class="col-lg-12">
-          <div class="card">
-            <div class="card-body table-responsive p-2">
+      </div>
+      @endif
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h1 class="m-0 text-dark">Edit Bank Account</h1>
+          </div>
+          <div class="card-body">
+            <form action="{{ route('bank-account.update',["bank_account"=>$bank_account->id]) }}" method="POST" enctype='multipart/form-data'>
+              @csrf
+              @method('PUT')
               <div class="row">
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="bank_account_name">Name</label>
-                    <div class="input-group">
-                      <input placeholder="eg. John Doe" value="{{$bank_account->name}}" type="text" class="form-control" im-insert="true" id="bank_account_name" name="bank_account_name" required>
+                <div class="col-lg-12">
+                  <div class="card">
+                    <div class="card-header card-form">
+                      Bank Account
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="bank_account_bank">Bank</label>
-                    <select class="js-example-basic-single js-states form-control" id="bank_account_bank" name="bank_account_bank" required>
-                        @include('owner.bank_account.bank')
-                    </select>
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="bank_account_number">Bank Account Number</label>
-                    <div class="input-group">
-                    <input placeholder="eg. 9876543xxxxx" value="{{$bank_account->account_number}}" type="text" class="form-control" im-insert="true" id="bank_account_number" name="bank_account_number" required>
+                    <div class="card-body table-responsive p-2">
+                      <div class="row">
+                        <div class="col-lg-6">
+                          <div class="form-group">
+                            <label for="bank_account_name">Name</label>
+                            <div class="input-group">
+                              <input placeholder="eg. John Doe" value="{{$bank_account->name}}" type="text" class="form-control" im-insert="true" id="bank_account_name" name="bank_account_name" required>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-lg-6">
+                          <div class="form-group">
+                            <label for="bank_account_bank">Bank</label>
+                            <select class="js-example-basic-single js-states form-control" id="bank_account_bank" name="bank_account_bank" required>
+                                @include('owner.bank_account.bank')
+                            </select>
+                          </div>
+                        </div>
+                        <div class="col-lg-6">
+                          <div class="form-group">
+                            <label for="bank_account_number">Bank Account Number</label>
+                            <div class="input-group">
+                            <input placeholder="eg. 9876543xxxxx" value="{{$bank_account->account_number}}" type="text" class="form-control" im-insert="true" id="bank_account_number" name="bank_account_number" required>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+                
+              </div>
+              
+              <div class="row">
+                <div class="col-12">
+                  <div class="float-right">
+                    <button type="reset" class="btn btn-default"><i class="fas fa-times"></i> Discard</button>
+                    <button type="submit" class="btn btn-success"><i class="far fa-save"></i> Save</button>
                   </div>
                 </div>
               </div>
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
-        </div>
-        
-      </div>
-      
-      <div class="row">
-        <div class="col-12">
-          <div class="float-right">
-            <button type="reset" class="btn btn-default"><i class="fas fa-times"></i> Discard</button>
-            <button type="submit" class="btn btn-success"><i class="far fa-save"></i> Save</button>
+              <!-- /.row -->
+            </form>
           </div>
         </div>
       </div>
-      <!-- /.row -->
-    </form>
+    </div>
   </div><!-- /.container-fluid -->
 </div>
 <!-- /.content -->
--- resources/views/owner/bank_account/index.blade.php
+++ resources/views/owner/bank_account/index.blade.php
@@ -17,8 +17,7 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-12">
-        <h1 class="m-0 text-dark">Bank Account 
-          <a class="btn btn-success" style="float:right;" href="{{route('bank-account.create')}}"><i class="fas fa-plus"></i> Add New Bank Account</a></h1>
+        <h1 class="m-0 text-dark"></h1>
       </div><!-- /.col -->
     </div><!-- /.row -->
   </div><!-- /.container-fluid -->
@@ -28,46 +27,59 @@
 <!-- Main content -->
 <div class="content">
   <div class="container-fluid">
-    <div class="row">
-    @forelse ($bank_accounts as $bank_account)
-      <div class="col-12 col-sm-6 col-md-6 col-lg-4">
-        <div class="card redeem-header-container {{($bank_account->is_default)?'default-card':''}}">
-          <div class="card-body">
-            <div class = "row">
-              <div class="col-12" style="margin-bottom: 20px;">
-                Name : {{$bank_account->name}}<br>
-                Bank : {{$bank_account->bank}}<br>
-                Bank Account Number : {{$bank_account->account_number}}<br> 
-              </div>
-              <div class="col-6">
-              <button class="btn btn-success default-button {{($bank_account->is_default)?'hidden':''}}" bank-account-id="{{$bank_account->id}}">Set As Default</button>
-              </div>
-              <div class="col-6 middle">
 
-                <form id="deleteId" action="bank-account/{{$bank_account->id}}" method="post">
-                  @method('DELETE')
-                  @csrf
-                  <button class="btn btn-danger" style="float: right; margin-left:5px; margin-right:5px;" type="submit"><i class="fas fa-trash"></i></button>
-                </form>
-                <a class="btn btn-info" style="float: right; margin-left:5px; margin-right:5px;" href="{{route('bank-account.edit',['bank_account'=>$bank_account->id])}}"><i class="fas fa-edit"></i></a>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h1 class="m-0 text-dark">Bank Account 
+              <a class="btn btn-success" style="float:right;" href="{{route('bank-account.create')}}"><i class="fas fa-plus"></i> Add New Bank Account</a></h1>
+          </div>
+          <div class="card-body">
+            <div class="row">
+            @forelse ($bank_accounts as $bank_account)
+              <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="card redeem-header-container {{($bank_account->is_default)?'default-card':''}}">
+                  <div class="card-body">
+                    <div class = "row">
+                      <div class="col-12" style="margin-bottom: 20px;">
+                        Name : {{$bank_account->name}}<br>
+                        Bank : {{$bank_account->bank}}<br>
+                        Bank Account Number : {{$bank_account->account_number}}<br> 
+                      </div>
+                      <div class="col-6">
+                      <button class="btn btn-success default-button {{($bank_account->is_default)?'hidden':''}}" bank-account-id="{{$bank_account->id}}">Set As Default</button>
+                      </div>
+                      <div class="col-6 middle">
+
+                        <form id="deleteId" action="bank-account/{{$bank_account->id}}" method="post">
+                          @method('DELETE')
+                          @csrf
+                          <button class="btn btn-danger" style="float: right; margin-left:5px; margin-right:5px;" type="submit"><i class="fas fa-trash"></i></button>
+                        </form>
+                        <a class="btn btn-info" style="float: right; margin-left:5px; margin-right:5px;" href="{{route('bank-account.edit',['bank_account'=>$bank_account->id])}}"><i class="fas fa-edit"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
+              <!-- /.col-md-6 -->
+              @empty
+              <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="card">
+                  <div class="card-body">
+                    <h3>
+                      No Bank Account registered.
+                    </h3>
+                  </div>
+                </div>
+              </div>
+
+            @endforelse
             </div>
           </div>
         </div>
       </div>
-      <!-- /.col-md-6 -->
-      @empty
-      <div class="col-12 col-sm-6 col-md-6 col-lg-4">
-        <div class="card">
-          <div class="card-body">
-            <h3>
-            Please Add your Bank Account
-            </h3>
-          </div>
-        </div>
-      </div>
-
-    @endforelse
     </div>
     <!-- /.row -->
   </div><!-- /.container-fluid -->
